25be00008019c7115a660a6dd1faf07185e2f97d
mrzrow <mrzrow@yandex.ru>
2025-04-11 21:12:59 +0300

    updated file verification

--- src/app.py
+++ src/app.py
@@ -1,11 +1,19 @@
+import os
 from argparse import Namespace
 
 from src.reports import AbstractReport, HandlersReport
 
 class App:
     def __init__(self, args: Namespace):
-        self.files = args.files
+        self.files = self.get_files(args)
         self.report = self.get_report(args)
+
+    def get_files(self, args: Namespace) -> tuple[str, ...]:
+        files = args.files
+        for file in files:
+            if not os.path.exists(file):
+                raise FileNotFoundError(f"file not found: {file}")
+        return files
 
     def get_report(self, args: Namespace) -> AbstractReport:
         avaliable_reports = {
@@ -14,7 +22,7 @@ class App:
 
         report_type = args.report
         if report_type not in avaliable_reports:
-            raise ValueError('non-existent report type')
+            raise ValueError(f'non-existent report type: {report_type}')
         
         report = avaliable_reports[report_type]
         return report(self.files)
